d7b6674adff7218e178a19bb45a384aac21122a9
NgoQuangManh <quangmanh12082005@gmail.com>
2026-09-16 09:00:51 +0700

    Add media serving and update urlpatterns

--- library_system/urls.py
+++ library_system/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -20,7 +20,10 @@ urlpatterns = [
     path('recommendations/', include(('recommendations.urls', 'recommendations'), namespace='recommendations')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 
 if settings.DEBUG:
+    from django.conf.urls.static import static
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
